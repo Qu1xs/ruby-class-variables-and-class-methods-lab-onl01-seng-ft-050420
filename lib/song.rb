f86afe927fb767e6973ac125cdc127841ae1106f
kevin murphy <kevinnealmurphy@gmail.com>
2020-05-08 18:09:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,13 +28,13 @@ class Song
   
   def self.genre_count
     genre_count = Hash.new(0)
-    @@genres.each {|genre| genre_count[genre] += 1}
+    @@genres.each {|i| genre_count[i] += 1}
     genre_count
   end
 
   def self.artist_count
     artist_count = Hash.new(0)
-    @@artists.each {|artist| artist_count[artist] += 1}
+    @@artists.each {|i| artist_count[i] += 1}
     artist_count
   end
   
